e66e2bfda75d70758af5fa7b2cffbae629d416ae
finlopes97 <kyle.finlayson1997@gmail.com>
2024-06-15 07:40:57 +1000

    feat(collections): add content collections

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { z, defineCollection } from "astro:content";
+
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    tags: z.array(z.string())
+  })
+});
+
+export const collections = {
+  posts: postsCollection,
+};

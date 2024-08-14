ab4683dffda9cda84ae41bccd736dafa1655c034
finlopes97 <Kyle.Finlayson1997@gmail.com>
2024-08-14 21:15:44 +1000

    feat(seo): add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon(), react()],
+  site: 'https://justgood.games',
+  integrations: [tailwind(), mdx(), icon(), react(), sitemap()],
   vite: {
     ssr: {
       external: ["axios"]
